cd215d4cc2ea96fa6022593f87113ebb602db775
Stijn Swaanen <swaanenstijn@gmail.com>
2018-01-22 15:07:47 +0100

    Query result page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace TestConsumer\Controller;
 
+use CultuurNet\SearchV3\SearchClient;
 use CultuurNet\SearchV3\SearchQuery;
+use CultuurNet\SearchV3\Serializer\Serializer;
+use GuzzleHttp\Client;
 use TestConsumer\Credentials\Credentials;
 use TestConsumer\Form\CredentialsForm;
 use TestConsumer\Form\QueryForm;
@@ -43,7 +46,6 @@ class DefaultController extends Controller
     public function queryForm(Request $request, SessionInterface $session)
     {
         $credentials = $session->get('credentials');
-        //$searchQuery = new SearchQuery();
 
         $form = $this->createForm(QueryForm::class);
         $form->add('send', SubmitType::class, array(
@@ -64,12 +66,60 @@ class DefaultController extends Controller
                     unset($values[$key]);
                 }
             }
-            var_dump($values);
+
+            $searchQueryValues = array();
+            foreach ($values as $key => $searchQueryValue) {
+                if (is_array($searchQueryValue)) {
+                    foreach ($searchQueryValue as $arrayValue) {
+                        $searchQueryValues[] = $arrayValue;
+                    }
+                }
+                else {
+                    $searchQueryValues[] = $searchQueryValue;
+                }
+            }
+
+            $searchQuery = new SearchQuery(true);
+            foreach ($searchQueryValues as $parameter) {
+                $searchQuery->addParameter($parameter);
+            }
+            $queryString = $searchQuery->__toString();
+            $queryString = $credentials->getEndpoint() . '/offers?apiKey=' . $credentials->getKey() . '&' . $queryString;
+
+            $client = new Client([
+                'base_uri' => $credentials->getEndpoint(),
+                'timeout' => 5,
+                'headers' => [
+                    'X-Api-Key' => $credentials->getKey()
+                ]
+            ]);
+
+            $searchClient = new SearchClient($client, new Serializer());
+            $result = $searchClient->searchOffers($searchQuery);
+
+            $session->set('queryResult', $result);
+            $session->set('queryString', $queryString);
+
+            return $this->redirectToRoute('result');
         }
 
         return $this->render('default/query.html.twig', array(
             'credentials' => $credentials,
             'form' => $form->createView()
+        ));
+    }
+
+    public function queryResult(Request $request, SessionInterface $session)
+    {
+        $result = $session->get('queryResult');
+        $queryString = $session->get('queryString');
+
+        $serializer = new Serializer();
+        $serializedResult = $serializer->serialize($result);
+
+        return $this->render('default/result.html.twig', array(
+            'result' => $serializedResult,
+            'queryString' => $queryString
         ));
     }
 }
